d5d52bdc25cfc975d43bbf66a9f411bb2478e4e4
ztl <2154848861@qq.com>
2025-12-18 20:32:12 +0800

    [zzz] 视频下载

--- include/business_layer/mqtt/mqtt_topics.h
+++ include/business_layer/mqtt/mqtt_topics.h
@@ -9,9 +9,12 @@
 #define UPDATE_CONFIG_TOPIC                    BOX_ID + "/device/config/update"
 #define GET_ALL_DEVICE_STATUS_TOPIC            BOX_ID + "/device/status/getall"
 #define DOOR_LOCK_CONTROL_TOPIC                BOX_ID + "/device/doorLook/control"
+#define DOWNLOAD_VIDEO                         BOX_ID + "device/video/download"
 
 #define RESULT_GET_REAL_IMAGE_TOPIC            BOX_ID + "/device/camera/getRealImage/result"
 #define RESULT_UPDATE_CONFIG_TOPIC             BOX_ID + "/device/config/update/result"
 #define RESULT_GET_ALL_DEVICE_STATUS_TOPIC     BOX_ID + "/device/status/getall/result"
 #define RESULT_DOOR_LOCK_CONTROL_TOPIC         BOX_ID + "/device/doorLook/control/result"
+#define RESULT_DOWNLOAD_VIDEO                  BOX_ID + "device/video/download/result"
+
 #endif // DEVICE_TOPICS_H
