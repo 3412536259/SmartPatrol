4d703310d7a656e52f67ebf923402ac2be4e0857
tzr <1745133166@qq.com>
2025-12-23 09:00:15 +0800

    报警任务

--- include/business_layer/mqtt/mqtt_topics.h
+++ include/business_layer/mqtt/mqtt_topics.h
@@ -14,4 +14,5 @@
 #define RESULT_UPDATE_CONFIG_TOPIC             BOX_ID + "/device/config/update/result"
 #define RESULT_GET_ALL_DEVICE_STATUS_TOPIC     BOX_ID + "/device/status/getall/result"
 #define RESULT_DOOR_LOCK_CONTROL_TOPIC         BOX_ID + "/device/doorLook/control/result"
+#define SENSOR_ALARM_TOPIC                     BOX_ID + "/device/sensor/alarm"
 #endif // DEVICE_TOPICS_H
